42a8501e21bb810c69f3cd98846a20920460081e
Molly Johnson <molly211@gmail.com>
2020-07-24 14:44:39 +0000

    apple example added

--- Modelling_script.R
+++ Modelling_script.R
@@ -35,12 +35,42 @@ theme.clean <- function(){
 
 # For a simple linear model
 
-install.packages("agridat")
 library(agridat)
 
 # Using a sample dataset from the agridat package
 
 apples <- agridat::archbold.apple
+head(apples)
+summary(apples)
+
+theme.clean <- function(){
+  theme_bw()+
+    theme(axis.text.x = element_text(size = 12, angle = 45, vjust = 1, hjust = 1),
+          axis.text.y = element_text(size = 12),
+          axis.title.x = element_text(size = 14, face = "plain"),             
+          axis.title.y = element_text(size = 14, face = "plain"),             
+          panel.grid.major.x = element_blank(),                                          
+          panel.grid.minor.x = element_blank(),
+          panel.grid.minor.y = element_blank(),
+          panel.grid.major.y = element_blank(),  
+          plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), units = , "cm"),
+          plot.title = element_text(size = 20, vjust = 1, hjust = 0.5),
+          legend.text = element_text(size = 12, face = "italic"),          
+          legend.position = "right")
+}
+
+apples$spacing2 <- as.factor(apples$spacing)
+
+library(ggplot2)
+
+(apples.p <- ggplot(apples, aes(spacing2, yield)) +
+    geom_boxplot(fill = "#CD3333", alpha = 0.8, colour = "#8B2323") + 
+    theme.clean() + 
+    theme(axis.text.x = element_text(size = 12, angle = 0)) +
+    labs(x = "Spacing (m)", y = "Yield (kg)"))
+
+apples.m <- lm(yield ~ spacing2, data = apples)
+summary(apples.m)
 
 # Using population trend data for the European Shag from the Living Planet Index
 # Import the shagLPI.csv file from the project's directory
